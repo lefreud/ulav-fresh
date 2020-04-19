b36ba66a5800fcc93059c0e0334c3b6d181306f8
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-04-19 07:33:24 -0400

    Added automated login checkbox to popup.

--- popupScripts/popup.js
+++ popupScripts/popup.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const checkbox = document.getElementById('dark-theme-checkbox');
+    const darkThemeCheckbox = document.getElementById('dark-theme-checkbox');
+    const automatedCheckbox = document.getElementById('automated-login-checkbox');
 
-    // Set current checkbox state
-    chrome.storage.sync.get(['darkThemeEnabled'], (storage) => {
+    // Set current checkboxes state
+    chrome.storage.local.get(['darkThemeEnabled', 'automatedLoginEnabled'], (storage) => {
         if (storage.darkThemeEnabled === true) {
-            checkbox.checked = true;
+            darkThemeCheckbox.checked = true;
+        }
+        if (storage.automatedLoginEnabled === true) {
+            automatedCheckbox.checked = true;
         }
     });
 
-    checkbox.addEventListener('click',
-            (event) => {
-                chrome.storage.sync.set({darkThemeEnabled: checkbox.checked});
+    darkThemeCheckbox.addEventListener('click', () => {
+            chrome.storage.local.set({darkThemeEnabled: darkThemeCheckbox.checked});
+        }
+    );
+    automatedCheckbox.addEventListener('click', () => {
+            chrome.storage.local.set({automatedLoginEnabled: automatedCheckbox.checked});
+            if (!automatedCheckbox.checked) {
+                chrome.storage.local.remove('loginCredentials');
             }
-        );
+        }
+    );
 });
